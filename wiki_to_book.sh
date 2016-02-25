4d0b719669d6e28edf86baacef9f8ccb579be311
xoracle <liungkejin@gmail.com>
2016-02-26 00:58:07 +0800

    update

--- wiki_to_book.sh
+++ wiki_to_book.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 #
+# Author: Kejin
+# Date  : 2016/02/24
+# EMail : liungkejin@foxmail.com
+#
+
+#
 # 此脚本的目的是为了将 github Wiki 页面转换为 gitbook
 # 即将 Home.md 转换为 SUMMARY.md, 并将相应的文件转入到相应等级的文件夹下,
 # 注意：
@@ -41,11 +47,6 @@
 #
 #
 
-#
-# Author: Kejin
-# Date  : 2016/02/24
-# EMail : liungkejin@foxmail.com
-#
 FILE_SUFFIX=(
     "asciidoc"
     "textile"
@@ -66,11 +67,10 @@ home_dir=".";
 home_md=${DEF_HOME_MD};
 out_dir=${DEF_OUT_DIR};
 
-# 标示 index 是否存在
-declare -a indent_flag=();
-# 保存标题结构数据
-declare -a title_struct=();
-global_var="";
+# 记录一组标题的缩进数据 [num_indent]="level:path";
+declare -a title_indent_info=();
+# 记录上一行信息 (num_line is_title num_indent content)
+# declare -a last_line_info=();
 
 # 根据名字找到对应的文件
 function get_file()
@@ -91,13 +91,73 @@ function get_file()
     done;
 }
 
+# 处理标题等级
+function process_ref_title()
+{
+    local
+}
+
+# 处理标题
+declare -a last_title_info=();
+declare -a cur_title_info=();
+function process_title()
+{
+    cur_title_info=();
+
+    local title="$1" num_indent="$2";
+    title="$(tr -d [:punct:] <<<"$title")";
+    # local i=0 ref_flag=0 char="" length=${#title};
+    # for ((i=0; i<length; ++i)); then
+    #     char="${title:$i:2}";
+    #
+    #     if ((ref_flag == 0)); then
+    #         if [[ "$char" == "[[" ]]; then
+    #             ((ref_flag==1));
+    #         fi;
+    #     fi;
+    # done;
+
+    local level=0 arr_size="${#title_indent_info[@]}";
+    local last_same="${title_indent_info[$num_indent]}";
+
+    local last_level=0 last_path="" title_path="";
+    if [[ -n "$last_same" ]]; then
+        last_level="${last_same%%:*}";
+        last_path="${last_same#*:}";
+
+        title_path="${last_path%\/*}/${title}";
+        ((level=last_level));
+    else if [[ -n "${last_title_info[@]}" ]]; then
+        last_level="${last_title_info[1]}";
+        last_path="${last_title_info[2]}";
+
+        title_path="${last_path%\/*}/${title}";
+        ((level=last_level+1));
+    fi;
+
+    title_indent_info[$num_indent]="${level}:${title_path}";
+    cur_title_info=("${level}" "${num_indent}" "${title_path}");
+
+    last_path="${out_dir}/$last_path";
+    title_path="${out_dir}/$title_path";
+
+    if [[ ! -d "$last_path" ]]; then
+        :; # TODO
+    fi;
+}
+
 # 处理 [[]] 里面引用的内容
 # 凡是没有找到同等级的标题, 如果存在上一个标题，那就是上一个标题的子标题
+declare -g result_return="";
+declare -g alias_name_return="";
+declare -g file_name_return="";
 function process_ref_name()
 {
-    global_var="";
+    result_return="";
+    alias_name_return="";
+    file_path_return="";
 
-    local ref_name="$1" is_title="$2" num_indent="$3" num_line="$4";
+    local ref_name="$1";
 
     local page_name="${ref_name#*|}";
     local alias_name="${ref_name%|$page_name}";
@@ -110,62 +170,23 @@ function process_ref_name()
     fi;
 
     local src_file="${home_dir}/${fname}" out_file="${out_dir}/${fname}";
-    if ! cp "${src_file}" "${out_file}"; then
-        return;
-    fi;
+    cp "${src_file}" "${out_file}" || return;
 
-    local flag=indent_flag[$num_indent]
-    local size="${#}"
-    if [[ -z "$flag" ]]; then
-        :;
-    fi;
-
-    # 找到最后一个比当前的 indent 多的路径
-    local t i=0 lev=0 indent=0 info="";
-    local level=0 arr_size="${#title_struct[@]}";
-    for ((i=arr_size-1; i >= 0; --i)); do
-        info="${title_struct[$i]}";
-
-        IFS=':';
-        read lev indent t <<<"${info}";
-        IFS='';
-
-        local name="${info#*;}";
-        local path="${out_dir}/${name}";
-        local dir="${path%.*}";
-
-        if ((num_indent < indent)); then
-            ((level=lev+1));
-            if [[ -d "${path}" ]]; then
-                mv "${out_file}" "$path" && fname="${path}/${fname}";
-            elif [[ -f "${path}" ]]; then
-                if mkdir -p "$dir"; then
-                    if mv "${path}" "$dir" && mv "${out_file}" "$dir"; then
-                        fname="${dir}/${fname}";
-                        title_struct[$i]="${lev}:${indent}:;${dir}";
-                    fi;
-                fi;
-            fi;
-            break;
-        elif ((num_indent == indent)); then
-            mv "${out_file}" "$dir";
-            break;
-        else
-            :;
-        fi;
-    done;
-
-    title_struct[${arr_size}]="${level}:${num_indent}:;${fname}";
-
-    global_var="[${alias_name}]($fname)";
+    file_name_return="$fname";
+    alias_name_return="$alias_name";
+    result_return="[${alias_name}]($fname)";
 }
 
 # 处理包含 [[]] 的行
 # 解析出两个数据
 #  1. 判断是不是一个标题, 如果是的话, 他的空格是多少个
 #  2. 找出 [[]] 里面的内容
+declare -a last_title_out_files=();
+declare -a cur_title_out_files=();
 function process_line()
 {
+    cur_title_out_files=();
+
     local line="$1" num_line="$2" result="";
     local i=0 char="" wchar="" length=${#line};
 
@@ -180,7 +201,8 @@ function process_line()
             if [[ "$wchar" = '* ' ]]; then
                 ((is_title = 1));
 
-                title="${line:$((i+2)):$((length-i-2))}";
+                process_title "${line:$((i+2)):$((length-i-2))}" "$num_indent";
+
                 result="${result}${wchar}";
                 ((i+=2));
                 continue;
@@ -202,8 +224,13 @@ function process_line()
         elif ((ref_beg == 1)); then
             if [[ "${wchar}" = "]]" ]]; then
                 ((ref_beg = 0));
-                process_ref_name "$ref_name" "$is_title" "$num_indent" "$num_line";
-                result="${result}${global_var}";
+                process_ref_name "$ref_name";
+                result="${result}${result_return}";
+
+                if ((is_title == 1)); then
+                    [[ -n "${file_name_return}" ]] && cur_title_out_files[${#cur_title_out_files[@]}]="${file_name_return}";
+                fi;
+
                 ((i+=2));
                 continue;
             fi;
@@ -214,21 +241,20 @@ function process_line()
             continue;
         fi;
 
+        ((is_title == 1)) && title="${title}${char}";
+
         result="${result}${char}";
         ((i+=1));
     done;
 
+    if ((is_title == 1)); then
+        process_ref_title ;
+
+        last_title_info=("${cur_title_info[@]}");
+        last_title_out_files=("${cur_title_out_files[@]}");
+    fi;
+
     echo "$result";
-    # if ((is_title==1)); then
-    #     if [[ -n "${ref_name}" ]]; then
-    #         echo "${num_indent}, ${ref_name}";
-    #     else
-    #         echo "${num_indent}, ${title}";
-    #     fi;
-    #
-    # else
-    #     echo "not a title";
-    # fi;
 }
 
 function main()
@@ -254,6 +280,7 @@ function main()
         echo "create output dir '$out_dir' failed!";
         exit -1;
     fi;
+    TEMP_FILE="${out_dir}/${TEMP_FILE}";
 
     home_dir="${home_md%\/*}";
     if [[ -z "$home_dir" || "$home_dir" = "$home_md" ]]; then
@@ -263,8 +290,8 @@ function main()
     IFS=''; # 为了读取空格
     local num_line=0;
     while read line; do
-        process_line "$line" "$num_line";
         ((num_line += 1));
+        process_line "$line" "$num_line";
     done< <(sed -e "s/\t/    /g" "$home_md");
 }
 
